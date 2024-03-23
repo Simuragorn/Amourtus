d03f15b70ef22ebd0788769cc012e021cd211455
Simuragorn <simuragorn@gmail.com>
2024-03-23 19:51:17 +0200

    Add movement blocker

---
+++ Assets/Scripts/Constants/LayersConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Constants
+{
+    public static class LayersConstants
+    {
+        public const string Default = "Default";
+        public const string Obstacle = "Obstacle";
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,9 @@
+using Assets.Scripts.Constants;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float minMovementDistance = 1f;
     [SerializeField] private float movementSpeed = 4f;
     [SerializeField] private float runningSpeed = 8f;
     [SerializeField] private SpriteRenderer spriteRenderer;
@@ -22,6 +24,16 @@ public class PlayerMovement : MonoBehaviour
         animator.SetBool(isRunningAnimatorKey, isRunning);
         var direction = new Vector2(horizontalAxis, 0);
         float speed = isRunning ? runningSpeed : movementSpeed;
-        transform.Translate(speed * Time.deltaTime * direction);
+        if (CanMove(direction))
+        {
+            transform.Translate(speed * Time.deltaTime * direction);
+        }
+    }
+
+    private bool CanMove(Vector2 direction)
+    {
+        int layerMask = LayerMask.GetMask(LayersConstants.Obstacle);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, minMovementDistance, layerMask);
+        return hit.collider == null;
     }
 }
